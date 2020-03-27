062940f981e6ec1f832546192ddc07dc3a50cb0f
root <jhana.gustave@gmail.com>
2020-03-27 17:46:41 -0400

    init repo

---
+++ businessManagement/src/components/crm/Registration.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+
+export default class Registration extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: "",
+            password: "",
+            password_confirmation: "",
+            RegistrationErrors: ""
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        console.log('handle change', event);
+
+    }
+
+
+    handleSubmit(event) {
+        console.log('form submitted');
+        event.preventDefault();
+    }
+
+    render() {
+     return (
+     <div>
+         <form onSubmit={this.handleSubmit}>
+         <input 
+         type="email" 
+         name="email" 
+         placeholder="Email" 
+         value={this.state.email}
+         onChange={this.handleChange} 
+         required 
+         />
+         </form>  
+     </div>
+     );
+    }
+}
